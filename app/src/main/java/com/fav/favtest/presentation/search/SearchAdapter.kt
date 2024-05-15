8161289c5c094824e1d61ceb5685a1fbfbff4c0e
glenntuyu <cgtuyu31@gmail.com>
2024-05-15 11:31:28 +0700

    feature: create MovieCard

--- app/src/main/java/com/fav/favtest/presentation/search/SearchAdapter.kt
+++ app/src/main/java/com/fav/favtest/presentation/search/SearchAdapter.kt
@@ -1,6 +1,7 @@
 package com.fav.favtest.presentation.search
 
 import com.fav.adapterdelegate.BaseCommonAdapter
+import com.fav.favtest.presentation.view.MovieCardAdapterDelegate
 
 /**
  * Created by glenntuyu on 15/05/2024.
@@ -9,6 +10,7 @@ class SearchAdapter() : BaseCommonAdapter() {
 
     init {
         delegatesManager
+            .addDelegate(MovieCardAdapterDelegate())
     }
 
     fun updateList(newList: List<Any>) {
---
+++ app/src/main/java/com/fav/favtest/presentation/view/MovieCardDataView.kt
@@ -0,0 +1,10 @@
+package com.fav.favtest.presentation.view
+
+/**
+ * Created by glenntuyu on 15/05/2024.
+ */
+data class MovieCardDataView(
+    val image: String = "",
+    val title: String = "",
+    val releaseDate: String = "",
+)
